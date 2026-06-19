041772b88f87d843713e3b717f3ba33348f84841
bhoomi456 <bhoomij0304@gmail.com>
2026-06-19 11:48:26 +0530

    Created migration hospital to doctor

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,5 @@
 class Doctor < ApplicationRecord
-  validates :name ,presence: true, uniqueness: true
-  validates :specialization, presence: true
+  # validates :name ,presence: true, uniqueness: true
+  # validates :specialization, presence: true
+  belongs_to :hospital
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_19_052231) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_19_061442) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "doctors", force: :cascade do |t|
     t.datetime "created_at", null: false
+    t.bigint "hospital_id", null: false
     t.string "name"
     t.string "specialization"
     t.datetime "updated_at", null: false
+    t.index ["hospital_id"], name: "index_doctors_on_hospital_id"
   end
 
   create_table "hospitals", force: :cascade do |t|
@@ -37,5 +39,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_19_052231) do
     t.index ["doctor_id"], name: "index_patients_on_doctor_id"
   end
 
+  add_foreign_key "doctors", "hospitals"
   add_foreign_key "patients", "doctors"
 end
